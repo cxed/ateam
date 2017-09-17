aaa6a4184e0b1c1c2a767f71bcc443c4034ca992
Chris X Edwards <cxe-ateam@xed.ch>
2017-09-16 20:37:31 -0700

    fix: Misc bug hunting

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -101,9 +101,24 @@ class TLDetector(object):
         
     #Newly introduced function calculating a normal beeline distance
     def distance(self, p1, p2):
-        assert hasattr(p1,'x') and hasattr(p1,'y') and hasattr(p2,'x') and hasattr(p2,'y')
-        delta_x = p1.x - p2.x
-        delta_y = p1.y - p2.y
+        if hasattr(p1,'x'):
+            p1x= p1.x
+        else:
+            p1x= p1[0]
+        if hasattr(p1,'y'):
+            p1y= p1.y
+        else:
+            p1y= p1[1]
+        if hasattr(p2,'x'):
+            p2x= p2.x
+        else:
+            p2x= p2[0]
+        if hasattr(p2,'y'):
+            p2y= p2.y
+        else:
+            p2y= p2[1]
+        delta_x = p1x - p2x
+        delta_y = p1y - p2y
         return math.sqrt(delta_x*delta_x + delta_y*delta_y)	
 
     def get_closest_waypoint(self, pose):
@@ -204,7 +219,7 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        if(not self.has_image):
+        if not self.has_image:
             self.prev_light_loc = None
             return False
 
@@ -239,7 +254,7 @@ class TLDetector(object):
         
         #Find where the vehicle is and safe it in car position
         light_positions = self.config['light_positions']
-        if(self.pose):
+        if self.pose:
             car_position = self.get_closest_waypoint(self.pose.pose)
             if car_position is not None:
                 self.last_car_position = car_position
@@ -248,16 +263,17 @@ class TLDetector(object):
         light_pos_wp = []
         if self.waypoints is not None:
             wp = self.waypoints
-            for i in range(len(light_positions)):
+            for light_position in light_positions:
                 # FIX: See note in get_closest_waypoint_light. l_pos can't be a list!
-                l_pos = self.get_closest_waypoint_light(wp, light_positions[i])
+                # cxe: I just changed distance function to handle both. Maybe a bad idea.
+                l_pos = self.get_closest_waypoint_light(wp, light_position)
                 light_pos_wp.append(l_pos)
             self.last_light_pos_wp = light_pos_wp
         else:
             light_pos_wp = self.last_light_pos_wp
             
         # valtgun get id of next light
-        if (self.last_car_position > max(light_pos_wp)):
+        if self.last_car_position > max(light_pos_wp):
              light_num_wp = min(light_pos_wp)
         else:
             light_delta = light_pos_wp[:]
@@ -266,18 +282,20 @@ class TLDetector(object):
 
         light_idx = light_pos_wp.index(light_num_wp)
         light = light_positions[light_idx]
-
-        light_distance = self.distance_light(light, self.waypoints.waypoints[self.last_car_position].pose.pose.position)
+        
+        # FIX: distance_light does not seem to be defined.
+        #light_distance = self.distance_light(light, self.waypoints.waypoints[self.last_car_position].pose.pose.position)
+        light_distance = self.distance(light, self.waypoints.waypoints[self.last_car_position].pose.pose.position)
         
         if light:
-            if (light_distance >= self.IGNORE_FAR_LIGHT):
+            if light_distance >= self.IGNORE_FAR_LIGHT:
                 return -1, TrafficLight.UNKNOWN
             else:
                 if self.simulator_debug_mode == 0:
                     simulated_light_number = None
-                    for i in len(self.lights):
-                        if (self.distance(light, self.lights[i].pose.pose.position)<1.0):
-                            state = self.lights[i].state
+                    for thislight in self.lights:
+                        if (self.distance(light, thislight.pose.pose.position)<1.0):
+                            state = thislight.state
                             return light_num_wp, state
                     return -1, TrafficLight.UNKNOWN
                 else:
