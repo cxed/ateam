58e129dbcdd06990772f90f7bd519c85d45b635b
Andreas-Jankl <jankl.andreas@googlemail.com>
2017-09-22 02:05:10 -0700

    Now also working with the bagfiles

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -44,7 +44,7 @@ class TLDetector(object):
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=0)
         # A publisher to show us the cropped images. Maybe disable when no longer needed.
         # Refer to styx/conf.py for more hints.
-        self.cropped_pub = rospy.Publisher("/crop_image",Image, queue_size=2) 
+        self.cropped_pub = rospy.Publisher("/crop_image",Image, queue_size=100) 
 
         self.bridge = CvBridge()
         self.light_classifier = TLClassifier()
@@ -58,10 +58,15 @@ class TLDetector(object):
         self.best_waypoint = 0
         self.last_car_position = 0
         self.last_light_pos_wp = []
-        self.IGNORE_FAR_LIGHT = 25.0
+        self.IGNORE_FAR_LIGHT_REAL = 21.5
+        self.IGNORE_FAR_LIGHT_SIMULATOR_DATA_COLLECTION = 25.0
         self.IGNORE_FAR_LIGHT_SIMULATOR = 50.0
-        self.simulator_debug_mode = 1
-        self.save_images_simulator = 1
+        self.IGNORE_LOW_DISTANCE_LIGHT_SIMULATOR = 1.0
+        self.simulator_debug_mode = 0
+        self.simulator_classifier_mode = 0
+        self.realimages_classifier_mode = 1
+        self.save_images_simulator = 0
+        self.save_images_real = 1
 
         rospy.spin()
 
@@ -83,7 +88,7 @@ class TLDetector(object):
 	#rospy.loginfo('[TLNode] Start of TL Node')
 	if(self.simulator_debug_mode==1):
 		light_wp, state = self.process_traffic_lights_simulation()
-	elif(self.simulator_debug_mode==0):
+	elif((self.realimages_classifier_mode==1 or self.simulator_classifier_mode==1) and (self.simulator_debug_mode==0)):
 		light_wp, state = self.process_traffic_lights()
 	#rospy.loginfo('[TLNode] End of process traffic lights with result' + str(state) + str(light_wp))
 
@@ -171,68 +176,6 @@ class TLDetector(object):
 
 	return Z 
 
-    def project_to_image_plane_2(self, point_in_world):
-        """Project point from 3D world coordinates to 2D camera image location
-
-        Args:
-            point_in_world (Point): 3D location of a point in the world
-
-        Returns:
-            x (int): x coordinate of target point in image
-            y (int): y coordinate of target point in image
-
-        """
-
-        fx = self.config['camera_info']['focal_length_x']
-        fy = self.config['camera_info']['focal_length_y']
-        image_width = self.config['camera_info']['image_width']
-        image_height = self.config['camera_info']['image_height']
-
-        # get transform between pose of camera and world frame
-        trans = None
-        try:
-            now = rospy.Time.now()
-            self.listener.waitForTransform("/base_link",
-                  "/world", now, rospy.Duration(1.0))
-            (trans, rot) = self.listener.lookupTransform("/base_link",
-                  "/world", now)
-
-        except (tf.Exception, tf.LookupException, tf.ConnectivityException):
-            rospy.logerr("Failed to find camera to map transform")
-
-        #TODO Use tranform and rotation to calculate 2D position of light in image
-        # Ouch... Then there's this:
-        # https://discussions.udacity.com/t/focal-length-wrong/358568/12
-
-        # Variable Naming Note: Abc --> A=object (Car or stopLight), b=coord system (world,zeroed,car), c=geom variable
-        #======== DONE: Need to get this information out of the input argument.
-        Lwx,Lwy,Lwz = point_in_world[0], point_in_world[1], 0
-        #======== DONE: Need to get this information out of its ROS package?
-        Cwx,Cwy,Cwz = self.pose.pose.position.x, self.pose.pose.position.y, self.pose.pose.position.z 
-	#AJankl calculated this via converting a quaternion to euler angles
-	x_=self.pose.pose.orientation.x
-	y_=self.pose.pose.orientation.y
-	z_=self.pose.pose.orientation.z
-	w_=self.pose.pose.orientation.w
-	Cwtheta = math.radians(self.Quaternion_toEulerianAngle(x_,y_,z_,w_)) # Cwtheta in radians! Used quaternion conversion for this
-        #L0x,L0y,L0z = Lwx-Cwx,Lwy-Cwy,Lwz-Cwz # Stoplight position for coords moved so car is at 0,0,0.
-        L0x,L0y,L0z = Lwx-Cwx,Lwy-Cwy,0 # Stoplight position for coords moved so car is at 0,0,0.
-        # Maybe check for L0y=0 conditions, already lined up (ahead or behind).
-        Lctheta = math.atan2(L0y,L0x) # Direction (radians) from car to stoplight when car is at 0,0,0.
-        LcR = math.sqrt(L0x*L0x + L0y*L0y) # Distance from stoplight to car.
-        Lcphi = Lctheta-Cwtheta # Angle between car bearing and angle to stoplight.
-        Lcx= LcR * math.cos(Lcphi) # How far stoplight is ahead of car.
-        Lcy= LcR * math.sin(Lcphi) # How far stoplight is laterally over from car's current path.
-        hw,hh= image_width/2, image_height/2 # Screen half width and half height.
-
-        # Simple screen projection given aligned system.
-        # https://en.wikipedia.org/wiki/3D_projection
-        y = hh + int(fy*L0z/Lcx) # Screen vertical = focal_len_vert * stoplight_dist_high / stoplight_dist_ahead
-        x = hw + int(fx*Lcy/Lcx) # Screen horiz = focal_len_horiz * stoplight_dist_over / stoplight_dist_ahead
-        if Lcx<0 or x<-hw or x>hw or y<-hh or y>hh:
-            return False #???? Or (-1,-1), basically, it's not on the screen.
-        else:
-            return (x,y)
 
     def project_to_image_plane(self, point_in_world):
         """Project point from 3D world coordinates to 2D camera image location
@@ -250,18 +193,6 @@ class TLDetector(object):
         fy = self.config['camera_info']['focal_length_y']
         image_width = self.config['camera_info']['image_width']
         image_height = self.config['camera_info']['image_height']
-
-        # get transform between pose of camera and world frame
-        trans = None
-        try:
-            now = rospy.Time.now()
-            self.listener.waitForTransform("/base_link",
-                  "/world", now, rospy.Duration(1.0))
-            (trans, rot) = self.listener.lookupTransform("/base_link",
-                  "/world", now)
-
-        except (tf.Exception, tf.LookupException, tf.ConnectivityException):
-            rospy.logerr("Failed to find camera to map transform")
 
         #TODO - DONE - Use tranform and rotation to calculate 2D position of light in image
 
@@ -283,7 +214,7 @@ class TLDetector(object):
         Lcy = (Lwy-self.pose.pose.position.y)*math.cos(math.radians(car_heading))-(Lwx-self.pose.pose.position.x)*math.sin(math.radians(car_heading))
 
 	#Object point is already in car coordinate system now
-        objectPoints = np.array([[float(Lcx), float(Lcy), 5.0]], dtype=np.float32)
+        objectPoints = np.array([[float(Lcx), float(Lcy), 0.0]], dtype=np.float32)
 
 	#set transfromations zero as everything is in car cosy
         rvec = (0,0,0)
@@ -301,7 +232,11 @@ class TLDetector(object):
         x = int(ret[0,0,0])
         y = int(ret[0,0,1])
 
-        return (x, y)
+	if x>(image_width) or y>(image_height):
+            	return (False, False) # basically, it's not on the screen.
+        else:
+        	return (x,y)
+
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
@@ -319,31 +254,43 @@ class TLDetector(object):
 
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        x, y = self.project_to_image_plane(light)
+	if (self.simulator_classifier_mode==1):
+        	x, y = self.project_to_image_plane(light)
+	if (self.realimages_classifier_mode==1):
+        	x, y =  684, 548
 
 	rospy.loginfo('[TLNode_Real] Projected points '+str(x)+' '+str(y))
 
         #TODO - DONE - use light location to zoom in on traffic light in image
-        
-        if ((x is None) or (y is None) or (x < 0) or (y<0) or
-            (x>self.config['camera_info']['image_width']) or (y>self.config['camera_info']['image_height'])):
+        if ((x is False) or (y is False)):
             return TrafficLight.UNKNOWN
         else:
             # Cropped for the classifier from Markus which would need to ingest bgr8 images that are of size 300x200 (Can be changed if needed)
-            cv_cropped_image = cv_image[(y-150):(y+150),(x-100):(x+100)]
-            # A publisher to show the cropped images. Enable definition in __init__ also.
-	    cv_marked_image = cv_image.copy()
- 	    cv_marked_image = cv2.drawMarker(cv_marked_image,(x,y),(0,0,255),markerType=cv2.MARKER_CROSS, markerSize=30, thickness=2, line_type=cv2.LINE_AA)
+	    #height, width, channels = cv_image.shape
+	    #rospy.loginfo('[TLNode_Real] Image size ' + str(height)+','+str(width)+','+str(channels),)
+	    cv_cropped_image = cv_image.copy()
+	    if (self.simulator_classifier_mode==1):
+            	cv_cropped_image = cv_image[(y-150):(y+150),(x-100):(x+100)]
+	    if (self.realimages_classifier_mode==1):
+        	cv_cropped_image = cv_image[(y-375):(y+375),(x-250):(x+250)]
+	    	cv_cropped_image = cv2.resize(cv_cropped_image,(200,300),interpolation = cv2.INTER_CUBIC)
+
 
 	    if(self.save_images_simulator==1):
             	self.time=time.clock()
 		path = '/home/student/Pictures/simulated/'
             	cv2.imwrite(path+str(int(self.time*1000))+'.jpg',cv_cropped_image)
 	    	rospy.loginfo('[TLNode_Real] Saved Image from simulator ')
-	    self.cropped_pub.publish(self.bridge.cv2_to_imgmsg(cv_cropped_image, "bgr8"))
-	    #self.cropped_pub.publish(cropped_image)
-            
 
+	    if(self.save_images_real==1):
+            	self.time=time.clock()
+		path = '/home/student/Pictures/bagfiles/'
+            	cv2.imwrite(path+str(int(self.time*1000))+'.jpg',cv_cropped_image)
+	    	rospy.loginfo('[TLNode_Real] Saved Image from bagfile ')
+	    
+	    # A publisher to show the cropped images.
+	    self.cropped_pub.publish(self.bridge.cv2_to_imgmsg(cv_cropped_image, "bgr8"))
+            
         #Get classification
         return self.light_classifier.get_classification(cv_cropped_image)
 
@@ -388,20 +335,39 @@ class TLDetector(object):
 		else:
 		    light_delta = light_pos_wp[:]
 		    light_delta[:] = [x - self.last_car_position for x in light_delta]
-		    light_num_wp = min(i for i in light_delta if i > 0) + self.last_car_position
+		    light_num_wp = min(i for i in light_delta if i >= 0) + self.last_car_position
 
 		light_idx = light_pos_wp.index(light_num_wp)
 		light = light_positions[light_idx]
+
+		#rospy.loginfo('[TLNode_Real] Light identified to be nearest to car: '+ str(light_idx) + ' at position ' + str(light))
+		#rospy.loginfo('[TLNode_Real] Belongs to waypoint: '+ str	(light_num_wp) + ' at position ' + str(self.waypoints.waypoints[light_num_wp].pose.pose.position))
+		rospy.loginfo('[TLNode_Real] Car is at waypoint: '+ str(car_position) + ' at waypoint position ' + str(wp.waypoints[car_position].pose.pose.position) + ' at car position ' + str(self.pose.pose.position))
 		
 		# FIX: distance_light does not seem to be defined.
 		#light_distance = self.distance_light(light, self.waypoints.waypoints[self.last_car_position].pose.pose.position)
 		light_distance = self.distance(light, self.waypoints.waypoints[self.last_car_position].pose.pose.position)
+		
+		target_light_too_far = 1
+		if(self.simulator_classifier_mode==1):
+			if light_distance <=self.IGNORE_FAR_LIGHT_SIMULATOR_DATA_COLLECTION:
+				target_light_too_far = 0
+		if(self.realimages_classifier_mode==1):
+			rospy.loginfo('[TLNode_Real] Waypoint difference: '+ str(light_num_wp - car_position))
+			rospy.loginfo('[TLNode_Real] Waypoint logic state: '+ str(light_distance <=self.IGNORE_FAR_LIGHT_REAL and (light_num_wp - car_position)>=-15 and (light_num_wp - car_position)<=-6))
+			if light_distance <=self.IGNORE_FAR_LIGHT_REAL and (((light_num_wp - car_position)>=-15 and (light_num_wp - car_position)<=-9) or ((light_num_wp - car_position)>30)):
+				 target_light_too_far = 0
+				 rospy.loginfo('[TLNode_Real] Classification to be invoked: ')
+
+
+		rospy.loginfo('[TLNode_Real] Distance to light: '+ str(light_distance))
         
 	#Fix changed handling of simulator. Not being done in this function anymore
         if light:
-            if light_distance >= self.IGNORE_FAR_LIGHT:
+            if  target_light_too_far == 1:
                 return -1, TrafficLight.UNKNOWN
             else:
+		rospy.loginfo('[TLNode_Real] Invoke classification')
                 state = self.get_light_state(light)
                 return light_num_wp, state
             
@@ -463,7 +429,7 @@ class TLDetector(object):
         	else:
             		light_delta = light_pos_wp[:]
             		light_delta[:] = [x - self.last_car_position for x in light_delta]
-            		light_num_wp = min(i for i in light_delta if i > 0) + self.last_car_position
+            		light_num_wp = min(i for i in light_delta if i >= 0) + self.last_car_position
 
         	light_idx = light_pos_wp.index(light_num_wp)
         	light = light_positions[light_idx]
@@ -480,7 +446,7 @@ class TLDetector(object):
         
 	#Fix changed handling of simulator. Not being done in this function anymore
         if light:
-            if light_distance >= self.IGNORE_FAR_LIGHT_SIMULATOR:
+            if light_distance >= self.IGNORE_FAR_LIGHT_SIMULATOR or light_distance <=self.IGNORE_LOW_DISTANCE_LIGHT_SIMULATOR:
                 return -1, TrafficLight.UNKNOWN
             else:
                 state = self.lights[light_idx].state
