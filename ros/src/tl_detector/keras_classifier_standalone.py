013412c3f37c1a85c9441225c0d6142360846ee2
Markus Meyerhofer <max@MacBook-Pro.local>
2017-09-30 02:36:15 +0200

    initial commit of Keras training and standalone classifier

---
+++ ros/src/tl_detector/keras_classifier_standalone.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import tensorflow as tf
+import numpy as np
+import cv2
+from tensorflow.contrib.layers import flatten
+from keras.models import load_model
+
+class TLClassifierKerasStandalone:
+    def __init__(self):
+        #TODO DONE load classifier
+        self.debug = False
+        self.capture_images = False
+        self.verbose = False
+        
+        self.model = load_model('keras_model.h5')
+        if self.debug:
+            print('[TL Classifier] constructor completed: ')
+
+    def normalize_image(self, image):
+        r, g, b = cv2.split(image)
+        r = (r - 128)/128
+        g = (g - 128)/128
+        b = (b - 128)/128
+        return cv2.merge((r, g, b))
+    
+    def get_classification(self, image):
+        """Determines the color of the traffic light in the image
+
+        Args:
+            image (cv::Mat): image containing the traffic light
+
+        Returns:
+            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+
+        """
+        #TODO implement light color prediction
+
+        if self.capture_images:
+            cv2.imwrite(self.imgPath+str(int(time.clock()*1000))+'.jpg', image)
+            print('[TLClassifier] Saved Image ... ')
+
+        if self.debug:
+            print('[TL Classifier] invoked... ')
+
+        if image.shape != (300, 200, 3):
+            print('[TL Classifier] image shape NOK: ' + str(image.shape))
+            return "UNKNOWN shape"
+            
+        assert image.shape == (300, 200, 3)
+        if self.debug:
+            print('[TL Classifier] assertion ok: ')
+        
+        choices = {0: "GREEN", 1: "YELLOW", 2: "RED", 3: "UNKNOWN"}
+
+        image = self.normalize_image(image)
+        res = None
+        res = cv2.resize(image, None,fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
+        image = res.reshape(1, 150, 100, 3)                  
+        prediction = self.model.predict_classes(image, verbose=0)
+        result = choices.get(prediction[0], "UNKNOWN")
+
+        if self.verbose:
+            print('[TL Classifier] ' + result + ' detected.')
+
+        return  result
